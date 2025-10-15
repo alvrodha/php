1e9291a49f6f84f080c6e781208e8422cd4839a5
alvrodha <alvaroredondorubio@gmail.com>
2025-10-15 23:45:18 +0200

    comienzo ejercicios05

---
+++ php/ejercicios05/nif.php
@@ -0,0 +1,16 @@
+<?php
+    $letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJ5-01</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ php/ejercicios05/registrar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJ5-02</title>
+</head>
+<body>
+    <div id="content">
+        <div id="header">
+
+        </div>
+        <div>
+            
+        </div>
+    </div>
+    
+</body>
+</html> 
